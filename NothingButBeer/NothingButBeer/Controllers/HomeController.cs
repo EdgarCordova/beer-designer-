147dcc7714a6b02c5a12a36a330b06cb9f23eec0
Edgar Cordova <Ecordova3935@gmail.com>
2016-07-20 00:42:06 -0600

    ability to connect to the DB

--- NothingButBeer/NothingButBeer/Controllers/HomeController.cs
+++ NothingButBeer/NothingButBeer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NothingButBeer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,46 @@ namespace NothingButBeer.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static string _connect = "Data Source=EDGARPC;Initial Catalog=ShipCompliant;Integrated Security=True";
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetRecipes()
+        {
+
+            List<Recipe> recipes = new List<Recipe>();
+
+            using (SqlConnection con = new SqlConnection(_connect))
+            {
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Recipes", con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader != null)
+                        {
+                            while (reader.Read())
+                            {
+                                Recipe recipe = new Recipe();
+                                recipe.Id = Convert.ToInt32(reader["Id"]);
+                                recipe.Title = reader["Title"].ToString();
+                                recipe.Description = reader["Description"].ToString();
+                                recipes.Add(recipe);           
+                            }
+                        }
+                    } // reader closed and disposed up here
+
+                } // command disposed here
+
+            } 
+            return View();
+
         }
 
         public ActionResult Recipes()
@@ -23,6 +62,7 @@ namespace NothingButBeer.Controllers
 
         public ActionResult About()
         {
+
             ViewBag.Message = "Your application description page.";
 
             return View();
